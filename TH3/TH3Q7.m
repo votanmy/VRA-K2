6937fadac3bd74285c566ee20a6df60d72f83efb
votanmy <32262083+votanmy@users.noreply.github.com>
2017-10-14 22:45:15 +0700

    Add Q7

---
+++ TH3/TH3Q7.m
@@ -0,0 +1,24 @@
+function TH3Q7(n)
+    imgTrainAll = loadMNISTImages('train-images.idx3-ubyte');
+    lblTrainAll = loadMNISTLabels('train-labels.idx1-ubyte');
+    imgTestAll = loadMNISTImages('t10k-images.idx3-ubyte');
+    lblTestAll = loadMNISTLabels('t10k-labels.idx1-ubyte');  
+    
+    Mdl = fitcknn(imgTrainAll',lblTrainAll);
+    
+    nTestImages = size(imgTestAll, 2);
+
+    if n>0 && n<10
+        count = 0;
+        for t=1:nTestImages
+            if(lblTestAll(t) == n)
+                imgTest = imgTestAll(:,t);
+                lblPredictTest = predict(Mdl,imgTest');
+                if(lblPredictTest ~= n)
+                    count = count+1;
+                end
+            end
+        end
+        fprintf('So luong du doan sai so %d: %d\n',n,count);
+    end
+end
